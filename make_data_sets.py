15f5282ba6671b3a7505720cc076082992c8b2a7
Steve Hansen <steven.e.hansen@gmail.com>
2020-06-17 21:00:29 -0700

    Move data set creation to make_data_sets.py.  Number all executable cells.

--- make_data_sets.py
+++ make_data_sets.py
@@ -5,6 +5,11 @@
 # friendly names of the program, which will be used in selection
 # widgets.
 
+# In the following line, ECHO_modules.DataSet refers to the file DataSet.py
+# while 'import DataSet' refers to the DataSet class within DataSet.py.
+
+from ECHO_modules.DataSet import DataSet
+
 data_sets = {}
 ds = DataSet( name='RCRA Violations', idx_field='ID_NUMBER', 
                 table_name='RCRA_VIOLATIONS', echo_type="RCRA",
@@ -43,6 +48,22 @@ ds = DataSet( name='Air Compliance', echo_type="AIR",
                 table_name='ICIS-AIR_FCES_PCES', idx_field='PGM_SYS_ID',
                 date_field='ACTUAL_END_DATE', date_format='%m-%d-%Y' )
 data_sets[ ds.name ] = ds
+ds = DataSet( name='Combined Air Emissions', echo_type=["GHG","TRI"],
+                table_name='POLL_RPT_COMBINED_EMISSIONS', idx_field='REGISTRY_ID',
+                date_field='REPORTING_YEAR', date_format='%Y' )
+data_sets[ ds.name ] = ds
+my_sql = "select * from `POLL_RPT_COMBINED_EMISSIONS` " + \
+            " where PGM_SYS_ACRNM = 'E-GGRT' and REGISTRY_ID in "
+ds = DataSet( name='Greenhouse Gases', echo_type="GHG",
+                table_name='POLL_RPT_COMBINED_EMISSIONS', idx_field='REGISTRY_ID',
+                date_field='REPORTING_YEAR', date_format='%Y', sql = my_sql )
+data_sets[ ds.name ] = ds
+my_sql = "select * from `POLL_RPT_COMBINED_EMISSIONS` " + \
+            " where PGM_SYS_ID = 'TRIS' and REGISTRY_ID in "
+ds = DataSet( name='Toxic Releases', echo_type="TRI",
+                table_name='POLL_RPT_COMBINED_EMISSIONS', idx_field='REGISTRY_ID',
+                date_field='REPORTING_YEAR', date_format='%Y', sql = my_sql )
+data_sets[ ds.name ] = ds
 ds = DataSet( name='Water Quarterly Violations', echo_type="NPDES",
                 table_name='NPDES_QNCR_HISTORY', idx_field='NPDES_ID',
                 date_field='YEARQTR', date_format='%Y' )
@@ -78,4 +99,5 @@ data_sets[ ds.name ] = ds
 ds = DataSet( name='SDWA Return to Compliance', echo_type="SDWA",
                 table_name='SDWA_RETURN_TO_COMPLIANCE', idx_field='PWSID',
                 date_field='FISCAL_YEAR', date_format='%Y' )
+data_sets[ ds.name ] = ds
 
